85409a46d9031f4c765bf74cc7fd1983a50783c6
Enrique Morales Padrón <e.moralespadron@gmail.com>
2018-07-24 19:31:45 +0200

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Enrique Morales Padrón on 24/7/18.
+//  Copyright © 2018 Enrique Morales Padrón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
